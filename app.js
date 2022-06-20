6147a010f1d6702b123ea1bcd3e1142ca01f2743
kushi96 <102799733+kushi96@users.noreply.github.com>
2022-06-20 19:44:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+const btns=document.querySelectorAll('.btn')
+const btnhigh=document.querySelector('.bntHigh')
+const fan1=document.querySelector('.fan1')
+const wings=document.querySelector('.mainWings')
+ 
+
+// btnhigh.addEventListener('mouseover',function(){
+   
+//     fan1.classList.add('mainWing')
+// })
+
+
+   
+
